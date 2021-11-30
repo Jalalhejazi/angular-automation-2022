346b74228781bc0a276d48e55890e04412efb8f6
Jalal Hejazi <Jalal.hejazi@gmail.com>
2021-11-30 09:40:32 +0100

    fix/appName

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  title = "Angular WebApp"
+  title = "WebApp"
   constructor() { }
 
   ngOnInit() {
